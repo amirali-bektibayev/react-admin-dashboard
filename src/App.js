f36450d02bed584a00304f5bb6ba44f33513c353
Amir <102328593+amirali-bektibayev@users.noreply.github.com>
2023-01-25 18:32:39 +0100

    render form scene

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SideBar from "./scenes/global/SideBar";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -23,6 +24,7 @@ function App() {
                 <Route path="/team" element={<Team />} />
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<Form />} />
               </Routes>
             </div>
           </div>
