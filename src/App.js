7938d20862126a73158e189cb8de27d6ed4dd696
Amir <102328593+amirali-bektibayev@users.noreply.github.com>
2023-01-25 18:49:06 +0100

    render faq

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import FAQ from "./scenes/faq";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -25,6 +26,7 @@ function App() {
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/form" element={<Form />} />
+                <Route path="/faq" element={<FAQ />} />
               </Routes>
             </div>
           </div>
