600d4e5237a3ddafe376b0183dc30100a314e6c7
Amir <102328593+amirali-bektibayev@users.noreply.github.com>
2023-01-25 16:15:21 +0100

    feat: create TopBar and add icons

---
+++ src/scenes/global/TopBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const TopBar = () => {
+  return <div></div>;
+};
+
+export default TopBar;
